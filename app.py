cdeea5576e059ace08b763bdae864eaf3429023b
Jakson Cunha <jaksoncunha1@gmail.com>
2026-03-25 00:50:10 +0000

    Add PostgreSQL with Docker Compose

--- app.py
+++ app.py
@@ -1,14 +1,48 @@
 from flask import Flask, jsonify
+import os
+import psycopg2
 
 app = Flask(__name__)
 
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_NAME = os.getenv("DB_NAME", "workflowdb")
+DB_USER = os.getenv("DB_USER", "workflowuser")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "workflowpass")
+DB_PORT = os.getenv("DB_PORT", "5432")
+
+
+def get_db_connection():
+    return psycopg2.connect(
+        host=DB_HOST,
+        database=DB_NAME,
+        user=DB_USER,
+        password=DB_PASSWORD,
+        port=DB_PORT
+    )
+
+
 @app.route("/")
 def home():
-    return "WorkFlowLab is running, Jack!"
+    return "WorkFlowLab is running"
+
 
 @app.route("/health")
 def health():
     return jsonify({"status": "ok"})
+
+
+@app.route("/users")
+def users():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute("SELECT id, name FROM users;")
+    rows = cur.fetchall()
+    cur.close()
+    conn.close()
+
+    result = [{"id": row[0], "name": row[1]} for row in rows]
+    return jsonify(result)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
